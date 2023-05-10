a7cfbe9f2edc1f573136c4e6a24e75c09a6a6d29
Rocio Monaco <rociomonaco@gmail.com>
2023-05-10 11:47:16 -0300

    add vite aliases config

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { ViteAliases } from 'vite-aliases'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
+export default defineConfig(() => {
+  return {
+    plugins: [react(), ViteAliases()],
+    resolve: {
+      extensions: ['.js', '.jsx']
+    }
+  }
 })
