58df320eedbcd1fa97bde8460571fcef45725643
Rocio Monaco <rociomonaco@gmail.com>
2023-05-23 11:16:10 -0300

    add logout service

--- front/src/services/auth.js
+++ front/src/services/auth.js
@@ -1,5 +1,9 @@
-import axiosPublic from '@services/axiosConfig'
+import { axiosPublic, axiosPrivate as axios } from '@services/axiosConfig'
 
 export const login = ({ email, password }) => {
   return axiosPublic.post('/login', { email, password })
+}
+
+export const logout = () => {
+  return axios.post('/logout')
 }
