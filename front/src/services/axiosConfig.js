dd6a400a6115635cc19fd4e7b47674318d7a5505
Rocio Monaco <rociomonaco@gmail.com>
2023-05-24 22:07:53 -0300

    fix request interceptor from axios

--- front/src/services/axiosConfig.js
+++ front/src/services/axiosConfig.js
@@ -3,14 +3,21 @@ import axios from 'axios'
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
-const createAxiosInstance = ({ url = API_BASE_URL, headers = {} }) => {
-  console.log(url)
+const createAxiosInstance = ({ url = API_BASE_URL }) => {
   const instance = axios.create({
     baseURL: url,
     headers: {
-      'Content-Type': 'application/json',
-      ...headers
+      'Content-Type': 'application/json'
     }
+  })
+
+  instance.interceptors.request.use((request) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      request.headers.Authorization = `Bearer ${token}`
+      request.headers.withCredentials = true
+    }
+    return request
   })
 
   instance.interceptors.response.use(
@@ -24,13 +31,8 @@ const createAxiosInstance = ({ url = API_BASE_URL, headers = {} }) => {
   return instance
 }
 
-const axiosPublic = createAxiosInstance({})
+const axiosPublic = createAxiosInstance()
 
-const header = {
-  withCredentials: true,
-  Authorization: `Bearer ${localStorage.getItem('token')}`
-}
-
-const axiosPrivate = createAxiosInstance({ header })
+const axiosPrivate = createAxiosInstance()
 
 export { axiosPublic, axiosPrivate as axios }
