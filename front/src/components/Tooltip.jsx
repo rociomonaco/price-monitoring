26f5ca87cf65403f11578a18f0ddfb334b32ae7d
Rocio Monaco <rociomonaco@gmail.com>
2023-05-30 16:28:36 -0300

    add Tooltip and made changePositionTooltip by viewport

---
+++ front/src/components/Tooltip.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useRef, useState } from 'react'
+
+export const Tooltip = ({
+  children,
+  title = 'Texto Informativo',
+  position = 'tooltip-top'
+}) => {
+  const [tooltipPosition, setTooltipPosition] = useState(position)
+  const triggerRef = useRef(null)
+
+  useEffect(() => {
+    const triggerCurrent = triggerRef.current
+    const handleOnMouseEnter = () => {
+      const tooltip = triggerCurrent.children[1].getBoundingClientRect()
+      const changePosition = changePositionTooltip(tooltip, window)
+      changePosition && setTooltipPosition(changePosition)
+    }
+
+    triggerCurrent.addEventListener('mouseover', handleOnMouseEnter)
+    return () => {
+      triggerCurrent.removeEventListener('mouseover', handleOnMouseEnter)
+    }
+  }, [])
+
+  const changePositionTooltip = (element, window) => {
+    switch (true) {
+      case element.bottom > window.innerHeight:
+        return 'tooltip-top'
+      case element.right > window.innerWidth:
+        return 'tooltip-left'
+      case element.top < 0:
+        return 'tooltip-bottom'
+      case element.left < 0:
+        return 'tooltip-right'
+      default:
+        return false
+    }
+  }
+
+  return (
+    <div>
+      <div className='group relative inline-block' ref={triggerRef}>
+        {children}
+        <div
+          className={`w-96 p-2 hidden group-hover:block transition-all absolute bg-white shadow-md z-10 rounded-md ${tooltipPosition}`}
+        >
+          <p className='text-sm overflow-auto'>{title}</p>
+          <div className='tooltip-arrow' />
+        </div>
+      </div>
+    </div>
+  )
+}
