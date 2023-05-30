c850cbe9a8a178db61d70bdff4a1ba0d1c343018
Rocio Monaco <rociomonaco@gmail.com>
2023-05-30 17:17:03 -0300

    clean simpleInput

--- front/src/components/form/input/SimpleInput.jsx
+++ front/src/components/form/input/SimpleInput.jsx
@@ -1,6 +1,5 @@
 import { Tooltip } from '@components/Tooltip'
 import { Info } from '@components/icons/Info'
-import React from 'react'
 
 export const SimpleInput = ({
   id,
@@ -10,16 +9,18 @@ export const SimpleInput = ({
   register,
   required = true,
   error,
-  tooltipTitle
+  tooltipText,
+  showRequired = false
 }) => {
   return (
     <div className='flex flex-col gap-y-1.5'>
-      <div className='flex justify-between'>
+      <div className='flex justify-between items-center'>
         <label className='text-xs font-medium text-gray-500' htmlFor={id}>
+          {showRequired && '* '}
           {label}
         </label>
-        {tooltipTitle && (
-          <Tooltip title={tooltipTitle}>
+        {tooltipText && (
+          <Tooltip text={tooltipText}>
             <Info className='cursor-pointer' width={12} height={12} />
           </Tooltip>
         )}
