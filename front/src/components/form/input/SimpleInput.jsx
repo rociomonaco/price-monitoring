9b86dd2044e72abacd7a2a5832208319a8771c1d
Rocio Monaco <rociomonaco@gmail.com>
2023-05-30 16:29:30 -0300

    add Tooltip at SimpleInput

--- front/src/components/form/input/SimpleInput.jsx
+++ front/src/components/form/input/SimpleInput.jsx
@@ -1,3 +1,5 @@
+import { Tooltip } from '@components/Tooltip'
+import { Info } from '@components/icons/Info'
 import React from 'react'
 
 export const SimpleInput = ({
@@ -7,24 +9,31 @@ export const SimpleInput = ({
   type = 'text',
   register,
   required = true,
-  error
+  error,
+  tooltipTitle
 }) => {
   return (
     <div className='flex flex-col gap-y-1.5'>
-      <label className='text-xs font-medium text-gray-500' htmlFor={id}>
-        {label}
-      </label>
-      <div className='relative'>
-        <input
-          id={id}
-          className={`border rounded-md w-full ${
-            error ? 'border-red-600' : 'border-gray-300'
-          } p-1.5 outline-none focus:border-gray-700 focus:border-2`}
-          type={type}
-          {...register(id, { required: required })}
-          placeholder={placeholder}
-        />
+      <div className='flex justify-between'>
+        <label className='text-xs font-medium text-gray-500' htmlFor={id}>
+          {label}
+        </label>
+        {tooltipTitle && (
+          <Tooltip title={tooltipTitle}>
+            <Info className='cursor-pointer' width={12} height={12} />
+          </Tooltip>
+        )}
       </div>
+
+      <input
+        id={id}
+        className={`border rounded-md w-full ${
+          error ? 'border-red-600' : 'border-gray-300'
+        } p-1.5 outline-none focus:border-gray-700 focus:border-2`}
+        type={type}
+        {...register(id, { required: required })}
+        placeholder={placeholder}
+      />
     </div>
   )
 }
