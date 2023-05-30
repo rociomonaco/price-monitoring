76021bdc2aabb8df575d025f275e10837085f1c7
Rocio Monaco <rociomonaco@gmail.com>
2023-05-30 19:41:58 -0300

    change style at productForm

--- front/src/pages/product/Product.jsx
+++ front/src/pages/product/Product.jsx
@@ -1,5 +1,8 @@
+import { BtnSubmit } from '@components/form/button/BtnSubmit'
 import { Checkbox } from '@components/form/input/Checkbox'
 import { SimpleInput } from '@components/form/input/SimpleInput'
+import { Loading } from '@components/loaders/Loading'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 export const Product = () => {
@@ -8,79 +11,94 @@ export const Product = () => {
     handleSubmit,
     formState: { errors }
   } = useForm()
+  const [isLoading, setIsLoading] = useState(false)
 
   return (
     <div className='w-full flex flex-col gap-y-8'>
       <h2 className='text-2xl font-medium text-gray-700'>Nuevo Producto</h2>
 
-      <form className='flex flex-col md:flex-row gap-5'>
-        <div className='w-full flex flex-col gap-y-2'>
-          <SimpleInput
-            id='nombre'
-            placeholder='Moladora'
-            register={register}
-            label='Nombre'
-            error={errors.nombre}
-            showRequired
-            tooltipText='No afecta a la búsqueda en Mercado Libre'
-          />
-          <SimpleInput
-            id='sku'
-            placeholder='MOL4545'
-            register={register}
-            label='SKU'
-            error={errors.sku}
-            tooltipText='Número de referencia único de producto. Es importante que sea el mismo código que se utiliza para uso interno en la empresa.'
-            showRequired
-          />
+      <form className='flex flex-col gap-y-5'>
+        <div className='flex flex-col md:flex-row gap-5'>
+          <div className='w-full flex flex-col gap-y-2'>
+            <SimpleInput
+              id='nombre'
+              placeholder='Moladora'
+              register={register}
+              label='Nombre'
+              error={errors.nombre}
+              showRequired
+              tooltipText='No afecta a la búsqueda en Mercado Libre'
+            />
+            <SimpleInput
+              id='sku'
+              placeholder='MOL4545'
+              register={register}
+              label='SKU'
+              error={errors.sku}
+              tooltipText='Número de referencia único de producto. Es importante que sea el mismo código que se utiliza para uso interno en la empresa.'
+              showRequired
+            />
 
-          <SimpleInput
-            id='pvp'
-            placeholder='4000'
-            register={register}
-            label='PVP'
-            error={errors.pvp}
-            type='number'
-            showRequired
-            tooltipText='Precio sugerido de la publicación'
-          />
-          <Checkbox
-            id='outPvp'
-            register={register}
-            label='Sólo fuera del PVP'
-          />
+            <SimpleInput
+              id='pvp'
+              placeholder='4000'
+              register={register}
+              label='PVP'
+              error={errors.pvp}
+              type='number'
+              showRequired
+              tooltipText='Precio sugerido de la publicación'
+            />
+            <Checkbox
+              id='outPvp'
+              register={register}
+              label='Sólo fuera del PVP'
+            />
+          </div>
+          <div className='w-full flex flex-col gap-y-2'>
+            <SimpleInput
+              id='descripcion'
+              register={register}
+              label='Descripción'
+              error={errors.descripcion}
+              tooltipText='Breve descripción, la cual no afectará en la búsqueda de Mercado Libre'
+              required={false}
+            />
+
+            <SimpleInput
+              id='terminosBusqueda'
+              register={register}
+              label='Términos de búsqueda'
+              error={errors.terminosBusqueda}
+              showRequired
+              tooltipText='Palabras para la búsqueda de Mercado Libre'
+            />
+            <SimpleInput
+              id='palabrasObligatorias'
+              register={register}
+              label='Palabras Obligatorias'
+              error={errors.palabrasObligatorias}
+              tooltipText='Palabras que sí o sí deben estar en la publicación'
+              required={false}
+            />
+            <SimpleInput
+              id='palabrasAExcluir'
+              register={register}
+              label='Palabras a Excluir'
+              error={errors.palabrasAExcluir}
+              tooltipText='Las publicaciones que contengan estas palabras no se incluyen'
+              required={false}
+            />
+          </div>
         </div>
-        <div className='w-full flex flex-col gap-y-2'>
-          <SimpleInput
-            id='descripcion'
-            register={register}
-            label='Descripción'
-            error={errors.descripcion}
-            tooltipText='Breve descripción, la cual no afectará en la búsqueda de Mercado Libre'
-          />
-
-          <SimpleInput
-            id='terminosBusqueda'
-            register={register}
-            label='Términos de búsqueda'
-            error={errors.terminosBusqueda}
-            showRequired
-            tooltipText='Palabras para la búsqueda de Mercado Libre'
-          />
-          <SimpleInput
-            id='palabrasObligatorias'
-            register={register}
-            label='Palabras Obligatorias'
-            error={errors.palabrasObligatorias}
-            tooltipText='Palabras que sí o sí deben estar en la publicación'
-          />
-          <SimpleInput
-            id='palabrasAExcluir'
-            register={register}
-            label='Palabras a Excluir'
-            error={errors.palabrasAExcluir}
-            tooltipText='Las publicaciones que contengan estas palabras no se incluyen'
-          />
+        <div className='w-1/4 self-end'>
+          <BtnSubmit isLoading={isLoading}>
+            {isLoading ? (
+              <Loading color={{ text: 'text-white', spinner: 'text-white' }} />
+            ) : (
+              'Agregar'
+            )}
+          </BtnSubmit>
         </div>
       </form>
     </div>
